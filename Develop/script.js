501704cc800088dd7292b536ff493cc281245b8f
Isaiah Negron <isaiahanegron@gmail.com>
2021-10-06 22:48:33 -0500

    added length requirement function

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,44 @@
 // Assignment code here
 
-
-// Get references to the #generate element
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
 var generateBtn = document.querySelector("#generate");
+
+//function generatePassword(){}
+
+function lengthCriteria() {
+  var passwordLength = window.prompt("How long do you want your password to be? (must be 8-128 characters");
+  if (passwordLength > 128){
+    alert("must be less than 128 characters!");
+    return lengthCriteria();
+  }
+  if (passwordLength < 8){
+    alert("must be greater than 8 characters!");
+    return lengthCriteria();
+  }
+  else {
+    alert("You selected " + passwordLength +" characters!");
+    return passwordLength;
+  }
+};
+
+function 
+
+
+
+
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
 
 // Write password to the #password input
 function writePassword() {
