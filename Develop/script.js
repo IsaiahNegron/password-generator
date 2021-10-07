9e93c29b812a66a60a7704e2fc857918efd49bef
Isaiah Negron <81210661+IsaiahNegron@users.noreply.github.com>
2021-10-07 11:51:18 -0500

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,5 @@
 // Assignment code here
 
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
 var newPassword = '';
 var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var lower = 'abcdefghijklmnopqurstuvwxyz';
@@ -94,14 +91,6 @@ function symbCriteria() {
 };
 
 
-
-
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
 
 // THEN a password is generated that matches the selected criteria
 // WHEN the password is generated
